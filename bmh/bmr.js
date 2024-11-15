4718c312b15bf4f06af7ec3511dd8fac770f3997
denizcanmaya <denizcanmaya@gmail.com>
2024-11-15 23:56:17 +0300

    bmh optimized, 1rm calculator added

--- bmh/bmr.js
+++ bmh/bmr.js
@@ -1,4 +1,4 @@
-document.querySelector('form').addEventListener('submit', function(event) {
+    document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault(); // Sayfanın yeniden yüklenmesini engellemek amaçlı!
     
     let age = document.getElementById("ageinput").value; // Yaş girdisini alabilmek için!
@@ -7,7 +7,10 @@ document.querySelector('form').addEventListener('submit', function(event) {
     let weight = document.getElementById("weightinput").value;
     
     let bmr;
-    
+
+    const bmrSonucDiv = document.getElementById("bmrSonucDiv");
+      bmrSonucDiv.style.display = "block"
+  
     // Karar Ağacı Yapısı Erkek seçilirse bu formül, kadın seçilirse bu!
     if (gender === "Male") {
       bmr = 10 * weight + 6.25 * height - 5 * age + 5;
@@ -17,6 +20,8 @@ document.querySelector('form').addEventListener('submit', function(event) {
     
     // Sonucun id'si belirli olan dive aktarılması!!
     let resultDiv = document.getElementById('bmrSonuc');
-    resultDiv.innerText = `Bazal Metabolik Hız: ${bmr.toFixed(2)} kcal/gün`;
+    resultDiv.innerHTML = `<strong>Bazal Metabolik Hız: ${bmr.toFixed(2)} kcal/gün</strong> <br>
+                <p class="mt-1">BMR (Bazal Metabolizma Hızı), vücudunuzun tamamen dinlenik haldeyken, yani uyurken ya da hareketsiz bir şekilde yatarken, hayatta kalmak için ihtiyaç duyduğu minimum enerji miktarını ifade eder. Bu enerji, temel yaşam fonksiyonlarını (kalp atışı, nefes alma, vücut sıcaklığını koruma, hücre yenilenmesi gibi) sürdürmek için gereklidir.</p>
+    `;
   });
   
