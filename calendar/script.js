3ceb61382cf7d793b8c45b445723e396cc53a928
denizcanmaya <denizcanmaya@gmail.com>
2025-03-10 16:55:12 +0300

    two new project added {auto-text-animation, dynamic calendar}

---
+++ calendar/script.js
@@ -0,0 +1,24 @@
+const monthEl = document.getElementById("month-name");
+const dayEl = document.getElementById("day-name");
+const dayNumber = document.getElementById("day-number");
+const year = document.getElementById("year");
+
+datePrint();
+
+setInterval(datePrint, 1000);
+
+function datePrint() {
+    const date = new Date();
+    monthEl.innerText = date.toLocaleString("en", {
+        month:"long"
+    })
+    
+    dayEl.innerText = date.toLocaleString("en", {
+        weekday: "long"
+    })
+    
+    dayNumber.innerText = date.getDate();
+    
+    year.innerText = date.toLocaleString();
+}
+
